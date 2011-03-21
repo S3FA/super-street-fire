ce4867a72b4ca5a08562d236f94d88f9deec1565
callumhay@gmail.com <callumhay@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2011-03-21 03:43:25 +0000

    ** Added the very preliminary beginnings of an python eclipse project - I've never worked with python before so if I got this wrong please feel free to trash it ** I figured we're going to need an ioserver module no matter what so I added that and started on defining some of the really basic input data structures that we would expect to see after parsing the serial data

---
+++ superstreetfire/src/ioserver/client_datatypes.py
@@ -0,0 +1,66 @@
+# client_datatypes.py
+# This file contains the basic data types that hold the various
+# expected inputs from the Super Street Fire game sensors
+
+
+# The GloveData class defines the data type the holds
+# the various inputs expected from the 9-DOF glove sensor
+# on each hand of the two players of the game.
+class GloveData:
+    def __init__(self, aX = 0, aY = 0, aZ = 0, 
+                       rX = 0, rY = 0, rZ = 0,
+                       hX = 0, hY = 0, hZ = 0):
+        # Acceleration data along the x, y and z axis
+        self.accelX = aX
+        self.accelY = aY
+        self.accelZ = aZ
+        # Gyro data - rotation about the x, y and z axis
+        self.rotX = rX
+        self.rotY = rY
+        self.rotZ = rZ
+        # Heading data - the directional vector,
+        # not necessarily normalized
+        self.headingX = hX
+        self.headingY = hY
+        self.headingZ = hZ
+    
+# Class for representing the head-set (EEG) data
+# TODO...
+class HeadsetData:
+    # TODO
+    
+    def __init__(self):
+        pass
+    
+    
+    
+# High level encapsulation of a single player's input
+# data - holds all the incoming data from both gloves
+# and their headset
+class PlayerData:
+    def __init__(self):
+        self.leftGloveData  = GloveData()
+        self.rightGloveData = GloveData()
+        self.headsetData    = HeadsetData()
+    
+
+# The Highest level representation of the input for the entire
+# Super Street Fire game - contains all input from all
+# relevant player sensor for a given capture of all
+# received packets at some instance during game play.
+class GameData:
+    def __init__(self):
+        self.player1Data = PlayerData()
+        self.player2Data = PlayerData()
+        pass
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
