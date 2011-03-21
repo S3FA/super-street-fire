fa570bed2fcb6cb3fb02e20a529a15fb9f79ca87
callumhay@gmail.com <callumhay@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2011-03-21 03:43:25 +0000

    ** Added the very preliminary beginnings of an python eclipse project - I've never worked with python before so if I got this wrong please feel free to trash it ** I figured we're going to need an ioserver module no matter what so I added that and started on defining some of the really basic input data structures that we would expect to see after parsing the serial data

---
+++ trunk/superstreetfire/src/ioserver/main.py
@@ -0,0 +1,13 @@
+
+if __name__ == '__main__':
+    
+    
+    from client_datatypes import GloveData, HeadsetData, PlayerData, GameData
+    g = GloveData()
+    h = HeadsetData()
+    p = PlayerData()
+    q = GameData()
+    
+    print g.accelX
+    
+    
