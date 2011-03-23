3b10709433f6004cac823e06340bfaec051d1b5a
callumhay@gmail.com <callumhay@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2011-03-23 15:12:20 +0000

    Figured out a better way to parse command line arguments

--- trunk/superstreetfire/src/superstreetfire.py
+++ trunk/superstreetfire/src/superstreetfire.py
@@ -9,14 +9,23 @@ The entry point for the superstreetfire server/application.
 #from ioserver.client_datatypes import *
     
 import sys    
-from settings import Settings
-    
-
-
-serialPort = None
-  
-
+#from settings import Settings
+from optparse import OptionParser
 
 if __name__ == '__main__':
-    appSettings = Settings(sys.argv)
-    print appSettings
+    #appSettings = Settings(sys.argv)
+    #print appSettings
+
+    # Parse options from the command line
+    #usage = "usage: %prog [options]"
+    argParser = OptionParser()
+    argParser.add_option("-i", "--input_port", action="store", type="string", dest="inputPort", \
+                         help="The serial port name/number that provides input from clients, this value can be omitted for debug.", \
+                         default="")
+    argParser.add_option("-o", "--output_port", action="store", type="string", dest="outputPort", \
+                         help="The serial port name/number that sends output to clients, this value can be omitted for debug.", \
+                         default="")
+
+    (options, args) = argParser.parse_args()
+    print options
+    
