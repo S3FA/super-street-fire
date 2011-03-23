38e913fcecf6e1313a873b5b937c76119f84314f
callumhay@gmail.com <callumhay@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2011-03-23 18:31:05 +0000

    Started adding a refactored class to act as the receiver for the ioserver

--- trunk/superstreetfire/src/superstreetfire.py
+++ trunk/superstreetfire/src/superstreetfire.py
@@ -4,28 +4,28 @@ superstreetfire.py
 The entry point for the superstreetfire server/application.
 '''
 
-#import re
-#import test
-#from ioserver.client_datatypes import *
-    
 import sys    
-#from settings import Settings
 from optparse import OptionParser
 
 if __name__ == '__main__':
-    #appSettings = Settings(sys.argv)
-    #print appSettings
 
     # Parse options from the command line
-    #usage = "usage: %prog [options]"
-    argParser = OptionParser()
+    usageStr = "usage: %prog [options]"
+    cmdLineDescStr = "The server for the superstreetfire application..."
+    argParser = OptionParser(usage=usageStr, description=cmdLineDescStr)
     argParser.add_option("-i", "--input_port", action="store", type="string", dest="inputPort", \
                          help="The serial port name/number that provides input from clients, this value can be omitted for debug.", \
-                         default="")
+                         default=None)
     argParser.add_option("-o", "--output_port", action="store", type="string", dest="outputPort", \
                          help="The serial port name/number that sends output to clients, this value can be omitted for debug.", \
-                         default="")
+                         default=None)
+    argParser.add_option("-b", "--baud_rate", action="store", type="int", dest="baudRate", \
+                         help="The serial port baud rate defaults to 57600.", default=57600)
 
     (options, args) = argParser.parse_args()
+
+
     print options
-    
+
+    # Spawn threads for listening and sending data over the serial port
+    # TODO
