e37d0d187d18edea4b3c3b8b0fad8e071a9e85eb
callumhay@gmail.com <callumhay@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2011-03-24 22:06:47 +0000

    Got a serial port emulator up and running and the parser is working, next line of business will be adding a synchronizing queue manager for the arriving asynchronous data

--- trunk/superstreetfire/src/ioserver/receiver.py
+++ trunk/superstreetfire/src/ioserver/receiver.py
@@ -1,61 +1,39 @@
 
-import Serial
+import serial
+import parser
+import threading
 
-class Receiver:
-    # We try to make parsing as fast as possible by using a hash table with 1H,1L,1R,etc.
-    # for keys and parse function references for values
-    PARSER_FUNCTION_DICT = {
-        '1L' : Player1LeftGloveParser,
-        '1R' : Player1RightGloveParser,
-        '1H' : Player1HeadsetSerialInputParser,
-        '2L' : Player2LeftGloveParser,
-        '2R' : Player2RightGloveParser,
-        '2H' : Player2HeadsetSerialInputParser
-    }
+class Receiver(threading.Thread):
 
     def __init__(self, inputSerialPort, baudRate):
-        self.serial = None
+        threading.Thread.__init__(self)
+        self.serial     = None
+        self.exitThread = False
         try:
             # NOTE: timeout=x means we wait up to x seconds to read from the serial port
-            self.serial = Serial.serial(inputSerialPort, baudrate=baudRate, timeout=1)
+            self.serialInputPort = serial.Serial(inputSerialPort, baudrate=baudRate, timeout=1)
         except serial.SerialException:
             print "ERROR: Serial port " + inputSerialPort + " was invalid/not found."
             exit(-1)
     
     def __del__(self):
-        if self.serial != None:
-            self.serial.close()
+        if self.serialInputPort != None:
+            self.serialInputPort.close()
+            
 
     def run(self):
         
         # Make sure this object is in a proper state before running...
-        if self.serial == None:
-            print "ERROR: Serial port " + inputSerialPort + " was invalid/not found, could not run receiver."
+        if self.serialInputPort == None:
+            print "ERROR: Serial port was invalid/not found, could not run receiver."
             return
         
         # Temporary variables used in the while loop
         currSerialDataStr = ""
-        
 
-        while True:
+        while not self.exitThread:
             # Listen for input on the serial port
-            currSerialDataStr = self.serial.readline()
-            self.ParseSerialData(currSerialDataStr)
+            currSerialDataStr = self.serialInputPort.readline()
+            parser.ParseSerialData(currSerialDataStr)
 
-            
-        
-
-    def ParseSerialData(self, serialDataStr):
-        # Break the serial input up based on the various known headers that
-        # designate our input sources...
-        splitInputList  = string.split(serialDataStr, ":")
-        splitListLength = len(splitInputList)
-
-        # splitListLength should likely be 2 here, but just for robustness
-        # we pretend like it could be longer
-        for i in range(0, splitListLength-1):
-            func = Receiver.PARSER_FUNCTION_DICT.get(splitInputList[i]);
-            if func != None:
-                # We're dealing with an expected package type from a client, parse it
-                # and queue it for the game simulation to deal with
-                func(splitInputList[i+1])
+        self.serialInputPort.close()
