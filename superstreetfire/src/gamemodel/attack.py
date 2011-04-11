e1b6233db0cc208d75f464d925e6fb2992fbe189
callumhay@gmail.com <callumhay@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2011-04-11 02:59:53 +0000

    Generalized the attack functionality in attack.py

--- superstreetfire/src/gamemodel/attack.py
+++ superstreetfire/src/gamemodel/attack.py
@@ -26,7 +26,8 @@ class Attack:
     # thickness:  The number of fire emitters thick that the attack is.
     # timeLength: The total time that the attack will take to travel
     #             from one side to the other of the arc.
-    def __init__(self, playerNum, sideEnum, thickness, timeLength):
+    # dmgPerFlame: The total damage per flame that hits the other player, in percent 
+    def __init__(self, playerNum, sideEnum, thickness, timeLength, dmgPerFlame):
         assert(timeLength > 0.0)
         assert(thickness >= 1)
         
@@ -36,18 +37,17 @@ class Attack:
         self._thickness      = thickness
         self._timeLength     = timeLength
         self._timePerEmitter = timeLength / float(SSFGame.NUM_FIRE_EMITTERS_PER_ARC)
+        self._dmgPerFlame    = dmgPerFlame
         
         # Track the amount of time that the attack has been active for
         self._currAttackTime       = 0.0
         
         # Track the amount of time that the current emitter has been on for so that
-        # We know when to shift the attack window
-        self._currDeltaLEmitterTime = 0.0
-        self._currDeltaREmitterTime = 0.0
-        
-        # Track the number of hits on either side
-        self._currNumLHits = 0
-        self._currNumRHits = 0 
+        # We know when to shift the attack window 
+        # We set these to be equal to the time per emitter to start - this will ensure
+        # that the window shifts on the first execution of the attack
+        self._currDeltaLEmitterTime = self._timePerEmitter
+        self._currDeltaREmitterTime = self._timePerEmitter
         
         # An attack is like a shift register - basically we keep a array that acts as the 
         # attack 'window', this window shifts across the relevant flame emitter arc
@@ -63,12 +63,12 @@ class Attack:
         # diminished and will then become Attack.INACTIVE_ATTACK_PART.
         #
         # Fire Emitter Arc:       |x|x|x|x|x|x|x|x|
-        # Attack Window:      |a|a|a| ---->
-        # Indices:            -2-1 0 1 2 3 4 5 6 7 
+        # Attack Window:    |a|a|a| ---->
+        # Indices:          -3-2-1 0 1 2 3 4 5 6 7 
         
         # The window index is always moving from lower to higher numbers
-        self._attackLWindowIdx  = -thickness + 1
-        self._attackRWindowIdx  = self._attackLWindowIdx
+        self._attackLWindowIdx  = -thickness
+        self._attackRWindowIdx  = -thickness
         self._leftAttackWindow  = None
         self._rightAttackWindow = None
     
@@ -76,10 +76,11 @@ class Attack:
         assert(self._lastLeftEmitters == None)
         assert(self._lastRightEmitters == None)
         
-        self._currAttackTime       = 0.0
-        self._currDeltaEmitterTime = 0.0
-        self._attackLWindowIdx     = -self._thickness + 1
-        self._attackRWindowIdx     = self._attackLWindowIdx
+        self._currAttackTime        = 0.0
+        self._currDeltaLEmitterTime = self._timePerEmitter
+        self._currDeltaREmitterTime = self._timePerEmitter
+        self._attackLWindowIdx      = -self._thickness
+        self._attackRWindowIdx      = -self._thickness
         
         self._currNumLHits = 0
         self._currNumRHits = 0
@@ -110,46 +111,62 @@ class Attack:
         self._currDeltaREmitterTime += dT
         
         if self._sideEnum == Attack.LEFT_SIDE:
-            self._LeftAttack(ssfGame, dT)
+            self._LeftAttack(ssfGame)
         elif self._sideEnum == Attack.RIGHT_SIDE:
-            self._RightAttack(ssfGame, dT)
+            self._RightAttack(ssfGame)
         elif self._sideEnum == Attack.LEFT_AND_RIGHT_SIDES:
-            self._LeftAttack(ssfGame, dT)
-            self._RightAttack(ssfGame, dT)
+            self._LeftAttack(ssfGame)
+            self._RightAttack(ssfGame)
         else:
             assert(False)
             
-    def _LeftAttack(self, ssfGame, dT):
-        assert(self._leftAttackWindow != None)
+    def _LeftAttack(self, ssfGame):
+        (self._attackLWindowIdx, self._currDeltaLEmitterTime) = \
+        self._Attack(ssfGame, self._leftAttackWindow, self._attackLWindowIdx, \
+                     ssfGame.leftEmitters, self._currDeltaLEmitterTime)
+
+    def _RightAttack(self, ssfGame):
+        (self._attackRWindowIdx, self._currDeltaREmitterTime) = \
+        self._Attack(ssfGame, self._rightAttackWindow, self._attackRWindowIdx, \
+                     ssfGame.rightEmitters, self._currDeltaREmitterTime)
+        
+    # Generalized Attack function - used to update the state of the attack whenever
+    # it is being executed/ticked.
+    # Returns: A tuple of the (updated attack window index, current delta emitter time)
+    def _Attack(self, ssfGame, attackWindow, attackWindowIdx, arcEmitters, deltaEmitterTime):
+        assert(attackWindow != None)
+        assert(arcEmitters  != None)
         assert(not self.IsFinished())
-    
-        windowLastIdx  = self._attackLWindowIdx + self._thickness
-        emitterArcSize = len(ssfGame.leftEmitters)
+        
+        windowLastIdx  = attackWindowIdx + self._thickness
+        emitterArcSize = len(arcEmitters)
+        assert(emitterArcSize == SSFGame.NUM_FIRE_EMITTERS_PER_ARC)
 
         # TRICKY STUFF: WE NEED TO CHECK THE ATTACK WINDOW TO SEE IF ANY HAVE SINCE BEEN DIMINISHED
         # BY A BLOCK THAT MAY HAVE OCCURRED IN THE MEANTIME
-        firstIdx = max(self._attackLWindowIdx, 0)
-        lastIdx  = min(emitterArcSize, windowLastIdx)
-        for i, j in zip(range(self._thickness), range(firstIdx, lastIdx)):
-            if self._leftAttackWindow[i] == Attack.ACTIVE_ATTACK_PART:
-                currEmitter = self._GetEmitter(ssfGame.leftEmitters, j)
-                # If the emitter is in the arc no longer holds an attack flame for self.playerNum
+        for i, j in zip(range(self._thickness), range(attackWindowIdx, windowLastIdx)):
+            # i is the index in the attack window
+            # j is the index of the emitter
+            if attackWindow[i] == Attack.ACTIVE_ATTACK_PART:
+                currEmitter = self._GetEmitter(arcEmitters, j)
+                # If the emitter in the arc no longer holds an attack flame for self.playerNum
                 # then it must have been extinguished by a block from the other player...
                 if currEmitter != None and currEmitter.HasAttackFlameOwnedByPlayer(self.playerNum):
-                    self._leftAttackWindow[i] = Attack.INACTIVE_ATTACK_PART
+                    attackWindow[i] = Attack.INACTIVE_ATTACK_PART
         
-        # Shift the left attack window if we've exceeded the emitter time
-        if self._currDeltaLEmitterTime >= self._timePerEmitter:
+        # Shift the attack window if we've exceeded the emitter time
+        if deltaEmitterTime >= self._timePerEmitter:
             # We'll first need to turn off the emitter that we're about to pass
-            passedEmitter = self._GetEmitter(ssfGame.leftEmitters, self._attackLWindowIdx)
+            passedEmitter = self._GetEmitter(arcEmitters, attackWindowIdx)
             # We only need to turn it off if the attack is still active on that part
             # and the emitter is actually inside the arc (it may be the case that the attack
             # is still starting up and has a thickness > 1)
-            if passedEmitter != None and self._leftAttackWindow[0] == Attack.ACTIVE_ATTACK_PART:
+            if passedEmitter != None and attackWindow[0] == Attack.ACTIVE_ATTACK_PART:
                 passedEmitter.FireOff(self.playerNum, FireEmitter.ATTACK_FLAME)
             
             # Shift the attack window
-            self._attackLWindowIdx += 1
+            attackWindowIdx += 1
+            windowLastIdx   += 1
             
             # Check to see if any new hits may have occurred...
             totalNumFinishedAttackParts  = windowLastIdx - emitterArcSize
@@ -157,42 +174,32 @@ class Attack:
                 # Go through all of the hits so far if we encounter new hits,
                 # each new hit will affect the game state...
                 for i in range(emitterArcSize, windowLastIdx):
-                    if self._leftAttackWindow[i] == Attack.ACTIVE_ATTACK_PART:
+                    if attackWindow[i] == Attack.ACTIVE_ATTACK_PART:
                         # A new attack was just landed
-                        ssfGame.Hurt(player.GetOtherPlayerNum(self.playerNum), 5)
-                        self._leftAttackWindow[i] = Attack.INACTIVE_ATTACK_PART
+                        ssfGame.Hurt(player.GetOtherPlayerNum(self.playerNum), self._dmgPerFlame)
+                        attackWindow[i] = Attack.INACTIVE_ATTACK_PART
             
-            
-            # If the attack window just shifted right off the end the of the emitter arc
+            # If the attack window just shifted completely off the end the of the emitter arc
             # then the attack is finished and we get out immediately
             if totalNumFinishedAttackParts == self._thickness:
-                self._currAttackTime = self._timeLength
-                return
+                assert(self._currAttackTime >= self._timeLength)
+                return (attackWindowIdx, deltaEmitterTime)
 
-            self._currDeltaLEmitterTime = self._currDeltaLEmitterTime - self._timePerEmitter
+            deltaEmitterTime = deltaEmitterTime - self._timePerEmitter
         
         # Go through the current attack window and update it based on the fire
-        # emitters that are being touched by the attack
-        firstIdx = max(self._attackLWindowIdx, 0)
-        lastIdx  = min(emitterArcSize, windowLastIdx)
-        for i, j in zip(range(self._thickness), range(firstIdx, lastIdx)):
-            if self._leftAttackWindow[i] == Attack.ACTIVE_ATTACK_PART:
-                currEmitter = self._GetEmitter(ssfGame.leftEmitters, j)
+        # emitters that are being touched by the attack:
+        for i, j in zip(range(self._thickness), range(attackWindowIdx, windowLastIdx)):
+            # i is the index in the attack window
+            # j is the index of the emitter
+            if attackWindow[i] == Attack.ACTIVE_ATTACK_PART:
+                currEmitter = self._GetEmitter(arcEmitters, j)
                 if currEmitter != None:
                     wasBlocked = currEmitter.FireOn(self.playerNum, FireEmitter.ATTACK_FLAME)
-                    self._leftAttackWindow[i] = wasBlocked
-
+                    attackWindow[i] = wasBlocked
     
-    def _RightAttack(self, ssfGame, dT):
-        assert(self._RightAttackWindow != None)
-
-        # Shift the right attack window if we've exceeded the amount of time per emitter... 
-        if self._currDeltaREmitterTime >= self._timePerEmitter:
-            
-            self._currDeltaREmitterTime = self._currDeltaREmitterTime - self._timePerEmitter
-
+        return (attackWindowIdx, deltaEmitterTime)
     
-
     def _GetEmitter(self, arcEmitters, idx):
         if idx < 0 or idx >= SSFGame.NUM_FIRE_EMITTERS_PER_ARC:
             return None
@@ -202,41 +209,14 @@ class Attack:
         else:
             return arcEmitters[-idx-1]
 
-'''
-    def _GetInitEmitters(self, arcEmitterList):
-        assert(not self.IsFinished())
-        
-        if self.playerNum == 1:
-            # The first player has emitter indices that go from the start to the
-            # end of an arc emitter list
-            return arcEmitterList[0:self._thickness]
-        else:
-            # The second player has emitter indices that go from the end to the
-            # start of an arc emitter list
-            return arcEmitterList[-1:-self._thickness:-1]
-    
-    def _GetCurrentEmitters(self, prevArcEmitterList):
-        startEmitterNum = int(self._currAttackTime / self._timePerEmitter)
-        if self.playerNum == 1:
-            # The first player has emitter indices that go from the start to the
-            # end of an arc emitter list
-            return prevArcEmitterList[startEmitterNum:self._thickness]
-        else:
-            # The second player has emitter indices that go from the end to the
-            # start of an arc emitter list
-            startEmitterNum -= 1
-            return prevArcEmitterList[-startEmitterNum:-self._thickness:-1]  
-'''
-
-    
 # Factory/Builder Methods for various Super Street Fire Attacks 
 def BuildLeftJabAttack(playerNum):
-    return Attack(playerNum, Attack.LEFT_SIDE, 1, 2.0)
+    return Attack(playerNum, Attack.LEFT_SIDE, 1, 2.0, 5)
 def BuildRightJabAttack(playerNum):
-    return Attack(playerNum, Attack.RIGHT_SIDE, 1, 2.0)
+    return Attack(playerNum, Attack.RIGHT_SIDE, 1, 2.0, 5)
 def BuildLeftHookAttack(playerNum):
-    return Attack(playerNum, Attack.LEFT_SIDE, 2, 3.0)
-def BuildRightHookAttack(playerNum):    
-    return Attack(playerNum, Attack.RIGHT_SIDE, 2, 3.0)
+    return Attack(playerNum, Attack.LEFT_SIDE, 2, 3.0, 5)
+def BuildRightHookAttack(playerNum):
+    return Attack(playerNum, Attack.RIGHT_SIDE, 2, 3.0, 5)
 def BuildHadoukenAttack(playerNum):
-    return Attack(playerNum, Attack.LEFT_AND_RIGHT_SIDES, 2, 4.0)
+    return Attack(playerNum, Attack.LEFT_AND_RIGHT_SIDES, 2, 4.0, 5)
