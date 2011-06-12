2af04c31e6aea7b96726e0306e2591bb287d9df8
syberchic@gmail.com <syberchic@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2011-06-12 20:48:26 +0000

    added placeholder for KO

--- trunk/superstreetfire/src/ioserver/output_listener.py
+++ trunk/superstreetfire/src/ioserver/output_listener.py
@@ -13,35 +13,42 @@ class SenderListener(GameModelListener):
         self.game = ssfGame
         self.sender = senderObj
         self.game.RegisterListener(self)
-        self.curTimer = '0'
-        self.p1Health = 100
-        self.p2Health = 100
+        self.curTimer = 0
+        self.p1Health = 0
+        self.p2Health = 0
     
     def Kill(self):
         self.game = None
         self.sender = None
             
     def OnTimerStateChanged(self, newTime):
-        timer = str(int(round(newTime)))
+        timer = int(round(newTime))
         if (timer != self.curTimer):
             self.curTimer = timer
-            #print 'send timer update ' + timer
+            #print 'send timer update ' + str(timer)
             self.sender.SendTimerNum(timer)
     
     def OnPlayerHealthChanged(self, players):
         if (players[0].GetHealth() != self.p1Health):
             self.p1Health = players[0].GetHealth()
-            #print 'send health p1: ' + str(players[0].GetHealth())
-            self.sender.SendLifeBarData( players[0].GetHealth(), players[1].GetHealth() )
+            if self.p1Health <= 0:
+                self.p1Health = 0
+                # person is dead.. send KO
+            #print 'send health p1: ' + str(self.p1Health) )
+            self.sender.SendLifeBarData( self.p1Health, self.p2Health )
+        
         if (players[1].GetHealth() != self.p2Health):
             self.p2Health = players[1].GetHealth()
-            #print 'send health p2: ' + str(players[1].GetHealth())
-            self.sender.SendLifeBarData( players[0].GetHealth(), players[1].GetHealth() )
+            if self.p2Health <= 0:
+                self.p2Health = 0
+                # person is dead.. send KO
+            #print 'send health p2: ' + str(self.p2Health)
+            self.sender.SendLifeBarData( self.p1Health, self.p2Health )
 
     def OnGameStateChanged(self, state):
         GameModelListener.OnGameStateChanged(self, state)
         cur_state = state.GetStateType()
-        # update KO, round won, etc
+        # update round and match won, etc
         if cur_state == ROUND_ENDED_GAME_STATE:
             print 'Sender ----- Round ' + str(state.roundNumber) + ' won by ' + str(state.roundWinner)
         if cur_state == MATCH_OVER_GAME_STATE:
