f1ffec1d511eee35ff3209848ac4b7938db9a7db
syberchic@gmail.com <syberchic@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2011-06-12 19:14:48 +0000

    sending round/match/health info

--- trunk/superstreetfire/src/ioserver/output_listener.py
+++ trunk/superstreetfire/src/ioserver/output_listener.py
@@ -5,6 +5,7 @@ Created on 2011-06-12
 '''
 
 from gamemodel.game_model_listener import GameModelListener
+from gamemodel.game_states import *
 
 class SenderListener(GameModelListener):
     def __init__(self, ssfGame, senderObj):
@@ -31,9 +32,20 @@ class SenderListener(GameModelListener):
         if (players[0].GetHealth() != self.p1Health):
             self.p1Health = players[0].GetHealth()
             #print 'send health p1: ' + str(players[0].GetHealth())
+            self.sender._sendLife( str(players[0].GetHealth()), str(players[1].GetHealth()) )
         if (players[1].GetHealth() != self.p2Health):
             self.p2Health = players[1].GetHealth()
             #print 'send health p2: ' + str(players[1].GetHealth())
+            self.sender._sendLife( str(players[0].GetHealth()), str(players[1].GetHealth()) )
+
+    def OnGameStateChanged(self, state):
+        GameModelListener.OnGameStateChanged(self, state)
+        cur_state = state.GetStateType()
+        # update KO, round won, etc
+        if cur_state == ROUND_ENDED_GAME_STATE:
+            print 'Sender ----- Round ' + str(state.roundNumber) + ' won by ' + str(state.roundWinner)
+        if cur_state == MATCH_OVER_GAME_STATE:
+            print 'Sender ----- Match won by ' + str(state.winnerPlayerNum)
                 
     def OnHWAddrChanged(self, hwaddr):
         pass
@@ -44,6 +56,3 @@ class SenderListener(GameModelListener):
     def OnPlayerMoves(self, actions):
         pass
 
-    def OnGameStateChanged(self, state):
-        pass
-       
