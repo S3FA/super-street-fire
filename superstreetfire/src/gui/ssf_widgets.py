5090f4943d3c927f2157b4dbdd84af0701d6aba8
greg.wiseman@gmail.com <greg.wiseman@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2011-06-16 04:02:22 +0000

    gui tweaks. much more to be done but now it's time for bed

---
+++ superstreetfire/src/gui/ssf_widgets.py
@@ -0,0 +1,30 @@
+from ocempgui.widgets import ImageLabel
+import pygame
+
+class EmitterWidget(ImageLabel):
+    
+    onColour = pygame.Color(200,200,200,255)
+    offColour = pygame.Color(128,128,128,128)
+    p1Colour = pygame.Color(200,0,0,255)
+    p2Colour = pygame.Color(0,200,200,255)
+    bothOnColour = pygame.Color(0,0,200,255)
+    
+    def __init__(self, w=16, h=16):
+        self.surface = pygame.Surface((w,h))
+        ImageLabel.__init__(self,self.surface)
+    
+    def update_emitter(self,emitter):
+        if emitter.flameIsOn:
+            if emitter.p1ColourIsOn and emitter.p2ColourIsOn:
+                self.surface.fill(EmitterWidget.bothOnColour)
+            if emitter.p1ColourIsOn:
+                self.surface.fill(EmitterWidget.p1Colour)
+            elif emitter.p2ColourIsOn:
+                self.surface.fill(EmitterWidget.p2Colour)
+            else:
+                self.surface.fill(EmitterWidget.onColour)
+        else:
+            self.surface.fill(EmitterWidget.offColour)
+        
+        self.set_dirty(True,False)
+    
