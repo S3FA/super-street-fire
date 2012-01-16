3ff7c545218c45f0b5b2570865fba8d7663a9e4b
greg.wiseman@gmail.com <greg.wiseman@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2012-01-16 03:15:35 +0000

    Initial commit didn't quite go perfectly - some files were left out and it seems we may in fact want the .project files checked in.

---
+++ trunk/ssf-java/FireEmitterModel/src/main/java/ca/site3/ssf/FireEmitterConfig.java
@@ -0,0 +1,18 @@
+package ca.site3.ssf;
+
+/**
+ * Contains configuration information for the fire emitters.
+ * 
+ * @author greg
+ */
+public class FireEmitterConfig {
+	
+	private boolean outerRingEnabled = true;
+	
+	private int numInnerEmittersPerSide = 8;
+	
+	private int numOuterEmittersPerSide = 8;
+	
+	
+	
+}
