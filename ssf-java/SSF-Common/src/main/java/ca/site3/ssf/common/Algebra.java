3c33113adc1510582bc119833c98de4915a221b4
callumhay@gmail.com <callumhay@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2012-02-25 23:47:04 +0000

    ** Added the .settings directories for eclipse ** Started adding basic gesture recognition stuff i.e., GloveData object ** Added basic algebra class to the common

---
+++ ssf-java/SSF-Common/src/main/java/ca/site3/ssf/common/Algebra.java
@@ -0,0 +1,17 @@
+package ca.site3.ssf.common;
+
+import org.apache.commons.math.geometry.Vector3D;
+
+public class Algebra {
+	public static final float  FLT_EPSILON = 0.0001f;
+	public static final double DBL_EPSILON = 0.00000001;
+	
+	private Algebra() {
+	}
+	
+	public static boolean Approx(Vector3D v1, Vector3D v2, double epsilon) {
+		return Math.abs(v1.getX() - v2.getX()) < epsilon &&
+			   Math.abs(v1.getY() - v2.getY()) < epsilon &&
+			   Math.abs(v1.getZ() - v2.getZ()) < epsilon;
+	}
+}
