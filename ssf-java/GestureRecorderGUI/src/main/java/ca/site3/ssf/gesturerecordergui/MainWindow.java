aa06e8b283e0cea0eda08c8cf82608e73c8c7858
everson.mike@gmail.com <everson.mike@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2012-03-04 00:43:46 +0000

    Added session logging to the UI, enhanced export options, framework for sending data to GestureRecognizer

--- ssf-java/GestureRecorderGUI/src/main/java/ca/site3/ssf/gesturerecordergui/MainWindow.java
+++ ssf-java/GestureRecorderGUI/src/main/java/ca/site3/ssf/gesturerecordergui/MainWindow.java
@@ -3,22 +3,24 @@ package ca.site3.ssf.gesturerecordergui;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-import ca.site3.ssf.ioserver.HardwareEvent;
+import ca.site3.ssf.gesturerecognizer.*;
+//import ca.site3.ssf.ioserver.HardwareEvent;
 
 // The main window for the recorder GUI.
-public class MainWindow extends JFrame implements HardwareEvent, ActionListener {
+public class MainWindow extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
-
+	private GestureData gesture = null;
 	private RecorderPanel recorderPanel   = null;
 	private ControlPanel controlPanel = null;
+	private LoggerPanel loggerPanel = null;
 	private boolean IsRecordMode = false;
 	
 	public MainWindow() {
@@ -26,7 +28,7 @@ public class MainWindow extends JFrame implements HardwareEvent, ActionListener 
 		
 		// Setup the frame's basic characteristics...
 		this.setTitle("Super Street Fire (Gesture Recorder GUI)");
-		this.setPreferredSize(new Dimension(700, 200));
+		this.setPreferredSize(new Dimension(900, 500));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
 		
@@ -38,13 +40,26 @@ public class MainWindow extends JFrame implements HardwareEvent, ActionListener 
 		
 		this.recorderPanel = new RecorderPanel();
 		this.controlPanel = new ControlPanel();
+		this.loggerPanel = new LoggerPanel();
 		
 		wrapperPanel.add(this.recorderPanel, BorderLayout.NORTH);
-		wrapperPanel.add(this.controlPanel, BorderLayout.SOUTH);
+		wrapperPanel.add(this.controlPanel, BorderLayout.CENTER);
+		wrapperPanel.add(this.loggerPanel, BorderLayout.SOUTH);
 		contentPane.add(wrapperPanel, BorderLayout.NORTH);
 		
 		this.pack();
 		this.setLocationRelativeTo(null);
+		
+		// Kick off the hardware event listener in the IOServer 
+		/* For debugging/testing purposes only */
+		this.recorderPanel.fileInfoPanel.isNewFile = true;
+		this.recorderPanel.fileInfoPanel.exportCsv.setState(true);
+		this.recorderPanel.fileInfoPanel.exportRecognizer.setState(true);
+		this.IsRecordMode = true;
+		for(int i = 0; i < 10; i++)
+		{
+			hardwareEventListener();
+		}
 	}
 
 	
@@ -68,33 +83,101 @@ public class MainWindow extends JFrame implements HardwareEvent, ActionListener 
         SwingUtilities.invokeLater(doCreateAndShowGUI);
 	}
 
-	// Triggered from IOServer. Set the coordinates data. If we're in record mode, save that data too.
-	public void actionPerformed(ActionEvent event) {
-//		if (event.getSource() == recordButtonPress from IOServer)
-//		{
-			// Currently we're assuming one record mode/trigger for both gloves, we can split it if needed
-			IsRecordMode = !IsRecordMode;
-//		}
-//		else if (event.getSource() == someHardwareEvent from IOServer)
-			// Populate the displays on the GUI with the data we're sent
-			this.recorderPanel.sensorDataPanelLeft.gyroscopeData.setText("0"); 
-			this.recorderPanel.sensorDataPanelLeft.magnetometerData.setText("0");
-			this.recorderPanel.sensorDataPanelLeft.accelerometerData.setText("0"); 
-			this.recorderPanel.sensorDataPanelRight.gyroscopeData.setText("0"); 
-			this.recorderPanel.sensorDataPanelRight.magnetometerData.setText("0"); 
-			this.recorderPanel.sensorDataPanelRight.accelerometerData.setText("0"); 
+	// Triggered from IOServer. Will most Set the coordinates data. If we're in record mode, save that data too.
+	// Should accept an IOServer.GloveData object from IOServer
+	public void hardwareEventListener() {
+		if (false)//event.getSource() == recordButtonPress from IOServer)
+		{
+			// Toggle record mode. Currently we're assuming one record mode/trigger for both gloves, we can split it if needed
+			this.IsRecordMode = !this.IsRecordMode;
+			this.recorderPanel.fileInfoPanel.isNewFile = true;
+			this.controlPanel.recordingLabel.setVisible(this.IsRecordMode);
+		}
+		else if (true){//event.getSource() == someHardwareEvent from IOServer)
 			
-			// If in record mode, write the data to the specified file
-			if (this.IsRecordMode){
-				this.recorderPanel.fileInfoPanel.recordFileInformation(this.recorderPanel.sensorDataPanelLeft.gyroscopeData.getText(),
-																		this.recorderPanel.sensorDataPanelLeft.magnetometerData.getText(),
-																		this.recorderPanel.sensorDataPanelLeft.accelerometerData.getText(),
-																		this.recorderPanel.sensorDataPanelRight.gyroscopeData.getText(),
-																		this.recorderPanel.sensorDataPanelRight.magnetometerData.getText(),
-																		this.recorderPanel.sensorDataPanelRight.accelerometerData.getText(),
-																		this.recorderPanel.fileInfoPanel.gestureName.getText() == "" ? "Unspecified" : this.recorderPanel.fileInfoPanel.gestureName.getText(),
-																		this.recorderPanel.fileInfoPanel.fileName.getText() == "" ? "RecorderData" : this.recorderPanel.fileInfoPanel.fileName.getText());
+			// Build the gesture coordinates object
+			BuildGestureObject();
+			DisplayAndLogData();
+			
+			// If we're recording, do some additional actions with the data object
+			if(this.IsRecordMode)
+			{
+				// Export to the gesture recognizer if selected
+				if (this.recorderPanel.fileInfoPanel.exportRecognizer.getState()){
+					SendDataToGestureRecognizer();
+				}
+				
+				// Write the results to a CSV file if selected
+				if (this.recorderPanel.fileInfoPanel.exportCsv.getState()){
+					this.recorderPanel.fileInfoPanel.recordFileInformation(this.gesture.gyroLeft,
+						this.gesture.magLeft,
+						this.gesture.accLeft,
+						this.gesture.gyroRight,
+						this.gesture.magRight,
+						this.gesture.accRight,
+						this.gesture.GestureName,
+						this.gesture.Time);
+				}
+				
+				this.recorderPanel.fileInfoPanel.isNewFile = false;
 			}
-//		}
+		}
+	}
+	
+	// Constructs a gesture object from the data sent by the IOServer
+	public void BuildGestureObject()
+	{
+		Calendar cal = Calendar.getInstance();
+	    SimpleDateFormat sdf = new SimpleDateFormat("H:mm:ss:SSS");
+	    
+		this.gesture = new GestureData();
+		this.gesture.gyroLeft = "0";
+		this.gesture.magLeft = "0";
+		this.gesture.accLeft = "0";
+		this.gesture.gyroRight = "0";
+		this.gesture.magRight = "0";
+		this.gesture.accRight = "0";
+		this.gesture.GestureName = this.recorderPanel.fileInfoPanel.gestureName.getText() == "" ? "Unspecified" : this.recorderPanel.fileInfoPanel.gestureName.getText();
+		this.gesture.Time = sdf.format(cal.getTime());
+	}
+	
+	// Displays the coordinates in the UI and logs anything recorded on screen
+	public void DisplayAndLogData()
+	{
+		// Populate the displays on the GUI with the data we're sent
+		this.recorderPanel.sensorDataPanelLeft.showCurrentData(this.gesture.gyroLeft, this.gesture.magLeft, this.gesture.accLeft);
+		this.recorderPanel.sensorDataPanelRight.showCurrentData(this.gesture.gyroRight, this.gesture.magRight, this.gesture.accRight);
+		
+		// Log the data on the UI
+		this.loggerPanel.logGestureData(this.gesture.gyroLeft,
+			this.gesture.magLeft,
+			this.gesture.accLeft,
+			this.gesture.gyroRight,
+			this.gesture.magRight,
+			this.gesture.accRight,
+			this.gesture.GestureName,
+			this.gesture.Time);
+	}
+	
+	// Get a string of instance data from the Gesture Recognizer
+	public void GetDataFromGestureRecognizer(){
+		GestureInstance gestureInstance = new GestureInstance();
+		//String gestureInstanceData = gestureInstance.toDataString();
+		
+		// Convert gestureInstanceData to a GestureData object. 
+		//TODO: What format does gesture instance need? 
+	}
+	
+	// Send a data string to the Gesture Recognizer
+	public void SendDataToGestureRecognizer(){
+		GestureInstance gestureInstance = new GestureInstance();
+		String formattedGestureData = "";
+		
+		// Convert the data to a string readable by the gesture recognizer
+		//TODO: How is this data string formatted?
+		
+		//gestureInstance.fromDataString(formattedGestureData);
+		
+		// Log and/or record the data?
 	}
 }
