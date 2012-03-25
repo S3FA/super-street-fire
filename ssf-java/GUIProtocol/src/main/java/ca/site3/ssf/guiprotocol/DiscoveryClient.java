d257128eceef6af49c6c9779c24ba24a554bb5e5
callumhay@gmail.com <callumhay@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2012-03-25 18:54:34 +0000

    Further improvements to the discovery client and server, added some comments.

--- ssf-java/GUIProtocol/src/main/java/ca/site3/ssf/guiprotocol/DiscoveryClient.java
+++ ssf-java/GUIProtocol/src/main/java/ca/site3/ssf/guiprotocol/DiscoveryClient.java
@@ -1,6 +1,5 @@
 package ca.site3.ssf.guiprotocol;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -15,11 +14,18 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import ca.site3.ssf.guiprotocol.Discovery.DiscoveryRequest.DiscoveryAppType;
 
-import com.google.protobuf.CodedInputStream;
-import com.google.protobuf.CodedOutputStream;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.UninitializedMessageException;
 
+/**
+ * The DiscoveryClient should reside on a client view application which is attempting
+ * to discover the ioserver on the network. The client has methods to either block or
+ * not block waiting to receive a discovery response from the ioserver.
+ * The response will contain all address information required to connect to the ioserver.
+ * 
+ * @author Callum
+ *
+ */
 public class DiscoveryClient extends Thread {
 	
 	private final long discoveryTimeInMs;
@@ -43,6 +49,12 @@ public class DiscoveryClient extends Thread {
 		assert(this.discoveryRequestPkg != null);
 	}
 
+	/**
+	 * A non-blocking check for any received discovery responses from the DiscoveryServer.
+	 * NOTE: This will pop any responses off the queue, so all calls past the first that didn't
+	 * return null will fail and return null.
+	 * @return A discovery response if there was one, null if no response.
+	 */
 	public Discovery.DiscoveryResponse getDiscoveryNoBlocking() {
 		Discovery.DiscoveryResponse result = null;
 		try {
@@ -52,6 +64,9 @@ public class DiscoveryClient extends Thread {
 			result = null;
 		}
 		
+		// Check for the special case where the discovery has terminated
+		// and no response was found (in this case an uninitialized response package
+		// is placed on the responseQueue).
 		if (result != null) {
 			if (!result.isInitialized()) {
 				result = null;
@@ -61,6 +76,12 @@ public class DiscoveryClient extends Thread {
 		return result;
 	}
 	
+	/**
+	 * A blocking/waiting check for any received discovery responses from the DiscoveryServer.
+	 * NOTE: This will pop any responses off the queue, so all calls past the first that didn't
+	 * return null will fail and return null.
+	 * @return A discovery response if there was one, null if no response.
+	 */
 	public Discovery.DiscoveryResponse getDiscoveryBlocking() {
 		if (!this.isAlive()) {
 			this.start();
@@ -74,6 +95,9 @@ public class DiscoveryClient extends Thread {
 			result = null;
 		}
 		
+		// Check for the special case where the discovery has terminated
+		// and no response was found (in this case an uninitialized response package
+		// is placed on the responseQueue).
 		if (result != null) {
 			if (!result.isInitialized()) {
 				result = null;
@@ -85,115 +109,131 @@ public class DiscoveryClient extends Thread {
 	
 	
 	public void run() {
-		
-		// Setup the socket for the client
 		try {
-			this.socket = new DatagramSocket();
-		}
-		catch (SocketException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		// Setup the multicast address for requesting a discovery from the server
-		InetAddress multicastAddr = null;
-		try {
-			multicastAddr = InetAddress.getByName(DiscoveryServer.DISCOVERY_MULTICAST_IP_ADDRESS);
-		}
-		catch (UnknownHostException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		// Request discovery...
-		byte[] requestBuffer = this.discoveryRequestPkg.toByteArray();
-		String bufferLengthStr = "" + requestBuffer.length;
-		byte[] bufferLengthBytes = bufferLengthStr.getBytes();
-		
-		DatagramPacket requestPacket1 = new DatagramPacket(bufferLengthBytes, bufferLengthBytes.length, multicastAddr, DiscoveryServer.DISCOVERY_SERVER_PORT);
-		DatagramPacket requestPacket2 = new DatagramPacket(requestBuffer, requestBuffer.length, multicastAddr, DiscoveryServer.DISCOVERY_SERVER_PORT);
-		
-		try {
-			this.socket.send(requestPacket1);
-			this.socket.send(requestPacket2);
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		// Now we wait for the specified discovery time for a response to arrive...
-		this.isListening = true;
-		
-		// Setup a timer to close the socket when the listen time has expired
-		this.listenTimer = new Timer();
-		this.listenTimer.schedule(new CloseSocketTask(), this.discoveryTimeInMs);
-		
-		this.responseQueue.clear();
-		boolean responseReceived = false;
-		DatagramPacket responsePacket = null;
-		
-		byte[] receiveBuffer1 = new byte[32];
-		byte[] receiveBuffer2 = null; 
-		while (this.isListening) {
-
+			// Setup the socket for the client
 			try {
-				responsePacket = new DatagramPacket(receiveBuffer1, receiveBuffer1.length);
-				this.socket.receive(responsePacket);
-				bufferLengthStr = new String(receiveBuffer1);
-				bufferLengthStr = bufferLengthStr.trim();
-				int buffer2Length = Integer.parseInt(bufferLengthStr);
-				receiveBuffer2 = new byte[buffer2Length];
-				responsePacket = new DatagramPacket(receiveBuffer2, receiveBuffer2.length);
-				this.socket.receive(responsePacket);
+				this.socket = new DatagramSocket();
+			}
+			catch (SocketException e) {
+				e.printStackTrace();
+				return;
+			}
+			
+			// Setup the multicast address for requesting a discovery from the server
+			InetAddress multicastAddr = null;
+			try {
+				multicastAddr = InetAddress.getByName(DiscoveryServer.DISCOVERY_MULTICAST_IP_ADDRESS);
+			}
+			catch (UnknownHostException e) {
+				e.printStackTrace();
+				return;
+			}
+			
+			// Request discovery...
+			byte[] requestBuffer = this.discoveryRequestPkg.toByteArray();
+			String bufferLengthStr = "" + requestBuffer.length;
+			byte[] bufferLengthBytes = bufferLengthStr.getBytes();
+			
+			DatagramPacket requestPacket1 = new DatagramPacket(bufferLengthBytes, bufferLengthBytes.length, multicastAddr, DiscoveryServer.DISCOVERY_SERVER_PORT);
+			DatagramPacket requestPacket2 = new DatagramPacket(requestBuffer, requestBuffer.length, multicastAddr, DiscoveryServer.DISCOVERY_SERVER_PORT);
+			
+			try {
+				this.socket.send(requestPacket1);
+				this.socket.send(requestPacket2);
 			}
 			catch (IOException e) {
-				continue;
+				e.printStackTrace();
+				return;
 			}
-			catch (NumberFormatException e) {
-				continue;
-			}
-
-			// We've received a response from the discovery server, add it to a synchronized queue...
-			Discovery.DiscoveryResponse discoveryResponsePkg;
-			try {
-				discoveryResponsePkg = Discovery.DiscoveryResponse.parseFrom(receiveBuffer2);
-				if (discoveryResponsePkg == null) {
+			
+			// Now we wait for the specified discovery time for a response to arrive...
+			this.isListening = true;
+			
+			// Setup a timer to close the socket when the listen time has expired
+			this.listenTimer = new Timer();
+			this.listenTimer.schedule(new CloseSocketTask(), this.discoveryTimeInMs);
+			
+			this.responseQueue.clear();
+			boolean responseReceived = false;
+			DatagramPacket responsePacket = null;
+			
+			byte[] receiveBuffer1 = new byte[32];
+			byte[] receiveBuffer2 = null; 
+			while (this.isListening) {
+	
+				try {
+					responsePacket = new DatagramPacket(receiveBuffer1, receiveBuffer1.length);
+					this.socket.receive(responsePacket);
+					bufferLengthStr = new String(receiveBuffer1);
+					bufferLengthStr = bufferLengthStr.trim();
+					int buffer2Length = Integer.parseInt(bufferLengthStr);
+					receiveBuffer2 = new byte[buffer2Length];
+					responsePacket = new DatagramPacket(receiveBuffer2, receiveBuffer2.length);
+					this.socket.receive(responsePacket);
+				}
+				catch (IOException e) {
 					continue;
 				}
-			}
-			catch (InvalidProtocolBufferException e) {
-				continue;
-			}
-
-			try {
-				if (this.responseQueue.add(discoveryResponsePkg)) {
-					responseReceived = true;
+				catch (NumberFormatException e) {
+					continue;
 				}
-				break;
-			}
-			catch (IllegalStateException e) {
-				break;
-			}
+	
+				// We've received a response from the discovery server, add it to a synchronized queue...
+				Discovery.DiscoveryResponse discoveryResponsePkg;
+				try {
+					discoveryResponsePkg = Discovery.DiscoveryResponse.parseFrom(receiveBuffer2);
+					if (discoveryResponsePkg == null) {
+						continue;
+					}
+				}
+				catch (InvalidProtocolBufferException e) {
+					continue;
+				}
+	
+				try {
+					if (this.responseQueue.add(discoveryResponsePkg)) {
+						responseReceived = true;
+					}
+					break;
+				}
+				catch (IllegalStateException e) {
+					break;
+				}
 
-		}
-		
-		if (!responseReceived) {
-			Discovery.DiscoveryResponse uninitPkg = null;
-			try {
-				uninitPkg = Discovery.DiscoveryResponse.newBuilder().buildPartial();
 			}
-			catch (UninitializedMessageException e) {
+			
+			// If no response was received then we place an uninitialized response package on the
+			// response queue - this prevents the client from blocking indefinitely on waiting for a response
+			// in the case where they make a call to 'getDiscoveryBlocking()'. The methods for getDiscovery* know
+			// to check for uninitialized responses and will simply return null in such cases.
+			if (!responseReceived) {
+				Discovery.DiscoveryResponse uninitPkg = null;
+				try {
+					uninitPkg = Discovery.DiscoveryResponse.newBuilder().buildPartial();
+				}
+				catch (UninitializedMessageException e) {
+				}
+				assert(!uninitPkg.isInitialized());
+				this.responseQueue.add(uninitPkg);
 			}
-			assert(!uninitPkg.isInitialized());
-			this.responseQueue.add(uninitPkg);
 		}
-		
-		this.socket.close();
-		this.listenTimer.cancel();
+		catch (Exception e) {
+			
+		}
+		finally {
+			this.socket.close();
+			this.listenTimer.cancel();
+			this.listenTimer.purge();
+		}
 	}
 	
-	
+	/**
+	 * Timer event class - used when the discovery timer has expired, this will execute its
+	 * run() method and end the discovery thread (by stopping it from listening) and close its socket.
+	 *
+	 * @author Callum
+	 *
+	 */
 	class CloseSocketTask extends TimerTask {
 		@Override
 		public void run() {
--- ssf-java/GUIProtocol/src/main/java/ca/site3/ssf/guiprotocol/DiscoveryServer.java
+++ ssf-java/GUIProtocol/src/main/java/ca/site3/ssf/guiprotocol/DiscoveryServer.java
@@ -41,99 +41,115 @@ public class DiscoveryServer extends Thread {
 	}
 
 	public void run() {
-		// Create a new multicast socket for receiving requests from discovery clients
 		try {
-			this.socket = new MulticastSocket(DiscoveryServer.DISCOVERY_SERVER_PORT);
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-			this.stopped = true;
-			return;
-		}
-		
-		// Join the multicast group
-		InetAddress multicastAddr = null;
-		try {
-			multicastAddr = InetAddress.getByName(DiscoveryServer.DISCOVERY_MULTICAST_IP_ADDRESS);
-		}
-		catch (UnknownHostException e) {
-			e.printStackTrace();
-			this.stopped = true;
-			return;
-		}
-		try {
-			this.socket.joinGroup(multicastAddr);
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-			this.stopped = true;
-			return;
-		}
-		
-		byte[] receiveBuffer1 = new byte[32];
-		byte[] receiveBuffer2 = null;
-		
-		while (!this.stopped) {
-		
-			// Block and wait for a discovery request package to be received by this server...
-			DatagramPacket requestPacket = new DatagramPacket(receiveBuffer1, receiveBuffer1.length);
+			// Create a new multicast socket for receiving requests from discovery clients
 			try {
-				
-				this.socket.receive(requestPacket);
-				
-				String bufferLengthStr = new String(receiveBuffer1);
-				bufferLengthStr = bufferLengthStr.trim();
-				int buffer2Length = Integer.parseInt(bufferLengthStr);
-				receiveBuffer2 = new byte[buffer2Length];
-				requestPacket = new DatagramPacket(receiveBuffer2, receiveBuffer2.length);
-				
-				this.socket.receive(requestPacket);
-		
+				this.socket = new MulticastSocket(DiscoveryServer.DISCOVERY_SERVER_PORT);
 			}
 			catch (IOException e) {
 				e.printStackTrace();
 				this.stopped = true;
-				break;
+				return;
 			}
 			
-			// Parse the discovery request package...
-			Discovery.DiscoveryRequest discoveryRequestPkg = null;
+			// Join the multicast group
+			InetAddress multicastAddr = null;
 			try {
-				discoveryRequestPkg = Discovery.DiscoveryRequest.parseFrom(receiveBuffer2);
-				System.out.println("Discovery Request from: " + discoveryRequestPkg.toString());
+				multicastAddr = InetAddress.getByName(DiscoveryServer.DISCOVERY_MULTICAST_IP_ADDRESS);
 			}
-			catch (InvalidProtocolBufferException e) {
-				System.err.println("Invalid discovery request datagram received: " + e.getMessage());
-				continue;
+			catch (UnknownHostException e) {
+				e.printStackTrace();
+				this.stopped = true;
+				return;
 			}
-			assert(discoveryRequestPkg != null);
-			
-			InetAddress requesterAddr = requestPacket.getAddress();
-			int requesterPort = requestPacket.getPort();
-			
-			byte[] sendBuffer = this.discoveryResponsePkg.toByteArray();
-			String bufferLengthStr = "" + sendBuffer.length;
-			byte[] bufferLengthBytes = bufferLengthStr.getBytes();
-			DatagramPacket responsePacket1 = new DatagramPacket(bufferLengthBytes, bufferLengthBytes.length, requesterAddr, requesterPort);
-			DatagramPacket responsePacket2 = new DatagramPacket(sendBuffer, sendBuffer.length, requesterAddr, requesterPort);
-			
 			try {
-				this.socket.send(responsePacket1);
-				this.socket.send(responsePacket2);
+				this.socket.joinGroup(multicastAddr);
 			}
 			catch (IOException e) {
 				e.printStackTrace();
 				this.stopped = true;
-				break;
+				return;
+			}
+			
+			byte[] receiveBuffer1 = new byte[32];
+			byte[] receiveBuffer2 = null;
+			
+			while (!this.stopped) {
+			
+				// Block and wait for a discovery request package to be received by this server...
+				DatagramPacket requestPacket = new DatagramPacket(receiveBuffer1, receiveBuffer1.length);
+				try {
+					
+					this.socket.receive(requestPacket);
+					
+					String bufferLengthStr = new String(receiveBuffer1);
+					bufferLengthStr = bufferLengthStr.trim();
+					int buffer2Length = Integer.parseInt(bufferLengthStr);
+					receiveBuffer2 = new byte[buffer2Length];
+					requestPacket = new DatagramPacket(receiveBuffer2, receiveBuffer2.length);
+					
+					this.socket.receive(requestPacket);
+			
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+					this.stopped = true;
+					break;
+				}
+				
+				// Parse the discovery request package...
+				Discovery.DiscoveryRequest discoveryRequestPkg = null;
+				try {
+					discoveryRequestPkg = Discovery.DiscoveryRequest.parseFrom(receiveBuffer2);
+					System.out.println("Discovery Request from: " + discoveryRequestPkg.toString());
+				}
+				catch (InvalidProtocolBufferException e) {
+					System.err.println("Invalid discovery request datagram received: " + e.getMessage());
+					continue;
+				}
+				assert(discoveryRequestPkg != null);
+				
+				InetAddress requesterAddr = requestPacket.getAddress();
+				int requesterPort = requestPacket.getPort();
+				
+				byte[] sendBuffer = this.discoveryResponsePkg.toByteArray();
+				String bufferLengthStr = "" + sendBuffer.length;
+				byte[] bufferLengthBytes = bufferLengthStr.getBytes();
+				DatagramPacket responsePacket1 = new DatagramPacket(bufferLengthBytes, bufferLengthBytes.length, requesterAddr, requesterPort);
+				DatagramPacket responsePacket2 = new DatagramPacket(sendBuffer, sendBuffer.length, requesterAddr, requesterPort);
+				
+				try {
+					this.socket.send(responsePacket1);
+					this.socket.send(responsePacket2);
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+					this.stopped = true;
+					break;
+				}
+			}
+			
+		}
+		catch (Exception e)  {
+		}
+		finally {
+			if (this.socket != null) {
+				this.socket.close();
 			}
 		}
-		
-		this.socket.close();
 	}
-	
+
+	/**
+	 * Stops the Discovery server, doesn't return until the server is terminated.
+	 */
 	public void stopServer() {
 		this.stopped = true;
 		this.socket.close();
+		try {
+			this.join();
+		}
+		catch (InterruptedException e) {
+		}
 	}
 
 	public static void main(String[] argv) {
@@ -149,6 +165,7 @@ public class DiscoveryServer extends Thread {
 		System.out.println("Running discovery server...");
 		DiscoveryServer discoveryServer = new DiscoveryServer(address.getHostAddress(), 45000);
 		discoveryServer.start();
+		discoveryServer.stop();
 	}
 	
 }
