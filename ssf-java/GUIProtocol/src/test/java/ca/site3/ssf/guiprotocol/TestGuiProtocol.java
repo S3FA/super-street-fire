2ac270e9cdeb7ae70855f84f4ec0e89b769df612
greg.wiseman@gmail.com <greg.wiseman@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2012-03-25 22:07:13 +0000

    parsing for legacy glove data

---
+++ ssf-java/GUIProtocol/src/test/java/ca/site3/ssf/guiprotocol/TestGuiProtocol.java
@@ -0,0 +1,16 @@
+package ca.site3.ssf.guiprotocol;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGuiProtocol {
+
+	
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
