fca6e7eb660598aa3256e1dd61993a84bb2f6162
everson.mike@gmail.com <everson.mike@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2012-06-15 17:26:18 +0000

    Got looping start/stop working correctly, made SoundPlayerController more portable, created a Tester class

--- trunk/ssf-java/Sound/src/main/java/ca/site3/ssf/Sound/SoundPlayerController.java
+++ trunk/ssf-java/Sound/src/main/java/ca/site3/ssf/Sound/SoundPlayerController.java
@@ -5,6 +5,8 @@ import java.util.Properties;
 
 import ca.site3.ssf.gamemodel.IGameModelEvent;
 import ca.site3.ssf.gamemodel.IGameModelListener;
+import ca.site3.ssf.gamemodel.PlayerAttackAction;
+import ca.site3.ssf.gamemodel.PlayerAttackActionEvent;
 
 /**
  * Listens for game events and plays sound effects and music as appropriate
@@ -12,45 +14,19 @@ import ca.site3.ssf.gamemodel.IGameModelListener;
  */
 public class SoundPlayerController implements IGameModelListener
 {
-	protected String resourcePath;
-	protected Properties configFile;
+	protected static String resourcePath;
+	protected String configPath = "SoundProperties.properties";
+	protected static Properties configFile;
 	protected Thread loopingSoundThread;
-	protected static IGameModelEvent event;
-	
-	/** Plays audio from given file names. */
-	//TODO: Replace main with a constructor. This only exists for testing purposes
-	public static void main( String [] args ) {
-		System.out.println("Started the SoundPlayerController.");
-	
-		// Test run of the audio file player. Test a non-looping sound
-		Thread soundThread = new Thread(new Runnable() {
-			public void run() {
-				PlaybackHandler.playAudioFile("Resources/Hadoken.wav", false);
-			}
-		});
-		
-		// Test a looping sound
-		Thread soundThread2 = new Thread(new Runnable() {
-			public void run() {
-				PlaybackHandler.playAudioFile("Resources/Sonic-boom.wav", true);
-			}
-		});
-		
-		// Start listening for and consuming the data from the gloves
-		soundThread.start();
-		soundThread2.start();
-	} 
+	public static IGameModelEvent event;
 
-    /**
-	 * Called for any event that can be listened for in the gamemodel.
-	 * @param event The object holding the event information.
-	 */
-	public void onGameModelEvent(IGameModelEvent gameModelEvent)
-	{	
+	// Sets up the config file and determines whether to loop the track to create an appropriate thread
+	public void playEventSounds(IGameModelEvent gameModelEvent)
+	{
 		this.setConfigFile();
 		this.event = gameModelEvent;
 		
-		// Currently assuming that GameStateChanged events are always changes to looping music. This logic will probably need to change.
+		// Need some condition to determine whether there is a looping track event. Currently assuming GameStateChangedEvents always loop
 		if (event.getType() == IGameModelEvent.Type.GAME_STATE_CHANGED)
 		{
 			// Interrupt the existing looping music thread if it exists
@@ -59,8 +35,7 @@ public class SoundPlayerController implements IGameModelListener
 			this.loopingSoundThread = new Thread(new Runnable() {
 				public void run() {
 					System.out.println("New thread created for a looping track.");
-					GameStateChangedSoundPlayer player = new GameStateChangedSoundPlayer();
-					player.playSounds(event);
+					onGameModelEvent(event);
 				}
 			});
 			
@@ -72,32 +47,7 @@ public class SoundPlayerController implements IGameModelListener
 			Thread soundThread = new Thread(new Runnable() {
 				public void run() {
 					System.out.println("New thread created for a sound effect.");
-					
-					if (event.getType() == IGameModelEvent.Type.PLAYER_ATTACK_ACTION)
-					{
-						PlayerAttackActionSoundPlayer player = new PlayerAttackActionSoundPlayer();
-						player.playSounds(event);
-					}
-					else if (event.getType() == IGameModelEvent.Type.ROUND_ENDED)
-					{
-						RoundEndedSoundPlayer player = new RoundEndedSoundPlayer();
-						player.playSounds(event);
-					}
-					else if (event.getType() == IGameModelEvent.Type.MATCH_ENDED)
-					{
-						MatchEndedSoundPlayer player = new MatchEndedSoundPlayer();
-						player.playSounds(event);
-					}
-					else if (event.getType() == IGameModelEvent.Type.RINGMASTER_ACTION)
-					{
-						RingmasterActionSoundPlayer player = new RingmasterActionSoundPlayer();
-						player.playSounds(event);
-					}
-					else if (event.getType() == IGameModelEvent.Type.ROUND_BEGIN_TIMER_CHANGED)
-					{
-						RoundBeginTimerChangedSoundPlayer player = new RoundBeginTimerChangedSoundPlayer();
-						player.playSounds(event);
-					}
+					onGameModelEvent(event);
 				}
 			});
 			
@@ -105,22 +55,62 @@ public class SoundPlayerController implements IGameModelListener
 		}
 	}
 	
-	// Interrupts the currently looping music thread if necessary
-	public void stopLoopingMusic()
-	{
-		if (this.loopingSoundThread != null)
+    /**
+	 * Called for any event that can be listened for in the gamemodel.
+	 * @param event The object holding the event information.
+	 */
+	public void onGameModelEvent(IGameModelEvent gameModelEvent)
+	{		
+		// Call the appropriate SoundPlayer based on the IGameModelEvent.Type
+		if (event.getType() == IGameModelEvent.Type.GAME_STATE_CHANGED)
 		{
-			this.loopingSoundThread.interrupt();
+			GameStateChangedSoundPlayer player = new GameStateChangedSoundPlayer();
+			player.playSounds(event);
+		}
+		else if (event.getType() == IGameModelEvent.Type.PLAYER_ATTACK_ACTION)
+		{
+			PlayerAttackActionSoundPlayer player = new PlayerAttackActionSoundPlayer();
+			player.playSounds(event);
+		}
+		else if (event.getType() == IGameModelEvent.Type.ROUND_ENDED)
+		{
+			RoundEndedSoundPlayer player = new RoundEndedSoundPlayer();
+			player.playSounds(event);
+		}
+		else if (event.getType() == IGameModelEvent.Type.MATCH_ENDED)
+		{
+			MatchEndedSoundPlayer player = new MatchEndedSoundPlayer();
+			player.playSounds(event);
+		}
+		else if (event.getType() == IGameModelEvent.Type.RINGMASTER_ACTION)
+		{
+			RingmasterActionSoundPlayer player = new RingmasterActionSoundPlayer();
+			player.playSounds(event);
+		}
+		else if (event.getType() == IGameModelEvent.Type.ROUND_BEGIN_TIMER_CHANGED)
+		{
+			RoundBeginTimerChangedSoundPlayer player = new RoundBeginTimerChangedSoundPlayer();
+			player.playSounds(event);
 		}
 	}
 	
-	private void setConfigFile()
+	// Interrupts the currently looping music thread if necessary
+	public void stopLoopingMusic()
+	{		
+		if (this.loopingSoundThread != null)
+		{
+			System.out.println("Interrupting the looping sounds thread.");
+			this.loopingSoundThread.stop();
+		}
+	}
+	
+	protected void setConfigFile()
 	{
 		this.configFile = new Properties();
 		
 		try
 		{
-			this.configFile.load(this.getClass().getClassLoader().getResourceAsStream("/SoundProperties.properties"));
+			this.configFile.load(this.getClass().getResourceAsStream(configPath));
 			this.resourcePath = configFile.getProperty("ResourcePath");
 		}
 		catch(IOException ex)
