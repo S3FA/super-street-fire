832fa70dd60b61e608ee80d1f0887713c2629cfb
greg.wiseman@gmail.com <greg.wiseman@gmail.com@204b579b-94fc-c64b-b3e6-851f78fbb48e>
2012-08-21 03:21:13 +0000

    glove info window now shows last update time.
    checking in from the plane on the way to reno :P

--- trunk/ssf-java/DevGUI/src/main/java/ca/site3/ssf/devgui/GloveDataInfoPanel.java
+++ trunk/ssf-java/DevGUI/src/main/java/ca/site3/ssf/devgui/GloveDataInfoPanel.java
@@ -2,16 +2,18 @@ package ca.site3.ssf.devgui;
 
 import java.awt.Color;
 import java.awt.GridBagLayout;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.ToolTipManager;
 import javax.swing.border.TitledBorder;
 
-import ca.site3.ssf.gamemodel.GameModel;
-import ca.site3.ssf.gamemodel.IGameModel;
-
+@SuppressWarnings("serial")
 class GloveDataInfoPanel extends JPanel {
 	
 	enum GloveType {
@@ -30,9 +32,13 @@ class GloveDataInfoPanel extends JPanel {
 	
 	private static final String NOT_CONNECTED_STR = "Not Connected";
 	
+	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
+	
 	private JLabel ipAddress;
 	private JProgressBar rssiMeter;
 	private JProgressBar batteryMeter;
+	private long lastUpdate = -1;
+	private JLabel lastUpdateLabel;
 	
 	GloveDataInfoPanel(GloveType gloveType) {
 		
@@ -54,6 +60,9 @@ class GloveDataInfoPanel extends JPanel {
 		this.batteryMeter.setStringPainted(true);
 		this.setBatteryPercent(0.0f);
 		
+		this.lastUpdateLabel = new JLabel();
+		this.setLastUpdateTime(-1);
+		
         GridBagLayout layout = new GridBagLayout();
 		this.setLayout(layout);
 		FormLayoutHelper formLayoutHelper = new FormLayoutHelper();
@@ -69,6 +78,12 @@ class GloveDataInfoPanel extends JPanel {
 		JLabel batteryLabel = new JLabel("Battery:");
 		formLayoutHelper.addLabel(batteryLabel, this);
 		formLayoutHelper.addLastField(this.batteryMeter, this);
+		
+		JLabel updatedLabel = new JLabel("Last Updated:");
+		formLayoutHelper.addLabel(updatedLabel, this);
+		formLayoutHelper.addLastField(this.lastUpdateLabel, this);
+		
+		ToolTipManager.sharedInstance().registerComponent(this);
 	}
 	
 	void setIPAddress(String ipAddressStr) {
@@ -88,6 +103,25 @@ class GloveDataInfoPanel extends JPanel {
 		ProgressBarColourLerp.setPercentageAndRedToGreenColour(this.rssiMeter, percent);
 	}
 	
+	void setLastUpdateTime(long time) {
+		this.lastUpdate = time;
+		if (time < 0) {
+			this.lastUpdateLabel.setText("[never]");
+		} else {
+			this.lastUpdateLabel.setText(DATE_FORMAT.format(new Date(time)));
+		}
+	}
 	
-	
+	@Override
+	public String getToolTipText() {
+		if (lastUpdate == -1) {
+			lastUpdate = 0;
+			return null; // weirdness due to setToolTipText. See http://stackoverflow.com/questions/6441242/java-swing-how-to-be-notified-if-tool-tip-is-about-to-become-visible
+		} else if (lastUpdate == 0) {
+			return "No updates";
+		} else {
+			long secondsAgo = Math.round((System.currentTimeMillis() - lastUpdate) / 1000);
+			return "Last heartbeat received " + secondsAgo + " seconds ago";
+		}
+	}
 }
